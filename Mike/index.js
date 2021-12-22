65031d38f4a8453222cdb94f78eb8f9dc6082236
Mike Ramsey <meramsey@gmail.com>
2021-12-21 20:10:18 -0600

    made another change, trying push and pull

--- Mike/index.js
+++ Mike/index.js
@@ -3,3 +3,9 @@ let lastName = 'Ramsey';
 console.log(firstName + ' ' + lastName + ' thinks he has a handle on this');
 
 console.log('making a change, trying a second commit');
+
+let favoriteNFLTeam = "Green Bay Packers";
+
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName + ' is a fan of the ' + favoriteNFLTeam);
